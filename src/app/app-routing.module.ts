a4d208989f0d5c448a0407bf9557b095603845d3
Gilberto Olivas <chivas-gilbert@hotmail.com>
2019-07-02 20:27:58 -0700

    login and register components generate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { AdminComponent } from './components/main/admin/admin.component';
 import { UserComponent } from './components/main/user/user.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { SensorsComponent as SensorsAdminComponent} from './components/main/admin/sensors/sensors.component';
 import { DashboardComponent as DashboardAdminComponent} from './components/main/admin/dashboard/dashboard.component';
 import { StationsComponent as StationsAdminComponent} from './components/main/admin/stations/stations.component';
@@ -13,6 +15,8 @@ import { StationsComponent as StationsUserComponent } from './components/main/us
 
 const routes: Routes = [
   { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'admin', component: AdminComponent,
     children: [
